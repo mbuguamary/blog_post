64584a64885ac198a5dd43e23462717ffc23048f
Mary Mbugua <mbuguamels@gmail.com>
2023-08-30 13:38:54 +0300

    sytled posts

---
+++ src/Pages/Posts/Posts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Posts.css"
+
+function Posts() {
+  return (
+    <div className='posts'>Posts
+    <div className='container'>
+        <h1>Posts</h1>
+    </div>
+    </div>
+    )
+}
+
+export default Posts

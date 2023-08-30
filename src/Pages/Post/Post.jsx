7f8a7ace168f89580189d236ab8ffa5e3a00740b
Mary Mbugua <mbuguamels@gmail.com>
2023-08-30 13:47:11 +0300

    added styles to post

--- src/Pages/Post/Post.jsx
+++ src/Pages/Post/Post.jsx
@@ -1,1 +1,16 @@
 
+import React from 'react'
+import "./Post.css"
+
+const Post = () => {
+  return (
+    <div className='post_wrapper'>
+    <div className='container'>
+        <h1> Single Post</h1>
+    </div>
+
+    </div>
+  )
+}
+
+export default Post

9d3ab1575161d02fe2d873ce205a374f39605449
Mary Mbugua <mbuguamels@gmail.com>
2023-09-01 15:29:13 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Posts from './Pages/Posts/Posts'
 import Post from './Pages/Post/Post'
+import {Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <Posts />
-      <Post />
-    </div>
+    <Routes>
+      <Route path='/' element={ <Posts />} />
+      <Route path='/post/:id' element={ <Post />} />
+   
+    </Routes>
   )
 }
 

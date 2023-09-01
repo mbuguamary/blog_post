da3e502981861196761ea836e7bd8dfbc357b6e7
Mary Mbugua <mbuguamels@gmail.com>
2023-09-01 15:28:34 +0300

    added update and post function

--- src/Pages/Post/Post.jsx
+++ src/Pages/Post/Post.jsx
@@ -1,12 +1,62 @@
-
+import config from "../../config.json"
 import React from 'react'
+import axios from 'axios'
 import "./Post.css"
+import { useParams } from 'react-router-dom'
+import { useState,useEffect } from 'react';
 
 const Post = () => {
-  return (
+    const {id} =useParams();
+    const [post,setPost] = useState({
+      title: "",
+      content:"",
+    });
+    useEffect(() => {
+        if (!id) return;
+        const fetchPost = async () => {
+          const { data } = await axios.get(`${config.apiUrl}/${id}`);
+          setPost(data);
+        };
+        fetchPost();
+      }, []);
+      const handleChange = (e) => {
+        const postClone = { ...post };
+        postClone[e.target.name] = e.target.value;
+        setPost(postClone);
+      };
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (id === "new") {
+          await axios.post(config.apiUrl, post);
+          return navigate("/");
+        } else {
+          await axios.put(`${config.apiUrl}/${id}`, post);
+          return navigate("/");
+        }
+      };
+  
+    return (
     <div className='post_wrapper'>
     <div className='container'>
-        <h1> Single Post</h1>
+       <form className="post">
+        <input
+        type='text'
+        placeholder='title...'
+        name="title"
+        value={post.title}
+        onChange={handleChange}
+        />
+        <input
+        type='text'
+        placeholder='title...'
+        name="title"
+        value={post.content}
+        onChange={handleChange}
+        />
+          <button onClick={handleSubmit} className="btn btn-primary">
+            {id === "new" ? "Post" : "Update"}
+          </button>
+       </form>
     </div>
 
     </div>
